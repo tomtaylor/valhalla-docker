5174cf69c98f36bdea4fda21ddafb6810aab54dd
Tom Taylor <tom@tomtaylor.co.uk>
2015-08-24 15:48:02 +0100

    Echo which repo is being fetched.

--- fetch-repos.sh
+++ fetch-repos.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 for repo in midgard baldr sif skadi mjolnir loki odin thor tyr; do
+  echo "Fetching $repo..."
   if [ ! -d $repo ]
   then
     git clone --recurse-submodules https://github.com/valhalla/$repo.git
